b0c75f1cdd993ecb25e1586acd34da9523a4c4b3
xiaoyiyi <xiaoyishikongde@163.com>
2019-01-27 23:53:13 +0800

    添加代码样式

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
         let n = 0
         let id = setInterval(()=>{
             n +=1
-            container.innerHTML = code.substring(0,n)
+            container.innerHTML = Prism.highlight(code.substring(0,n),Prism.languages.css)
             styleTag.innerHTML = code.substring(0,n)
             container.scrollTop = container.scrollHeight
             if(n >= code.length){
